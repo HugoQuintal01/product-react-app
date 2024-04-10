14a1cd9afa6853a67847dd90cf367452b08d7cb0
Hugo Quintal <hugoquintal@MBP-de-Hugo.lan>
2024-04-10 15:08:16 +0100

    Add basename props to browserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import Header from './components/header/Header';
-import Menu from './components/menu/Menu';
 import ProductList from './components/product-list/ProductList';
-import ProductPage from './products/[productId]/page'; // Import the ProductPage component
-import TopAnchor from './components/top-anchor/TopAnchor';
+import ProductPage from './products/[productId]/page';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <div className="App">
         <Routes>
           <Route path="/" element={<ProductList />} />
